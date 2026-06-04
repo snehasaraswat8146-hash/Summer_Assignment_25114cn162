879487a30c4805cbde10c8384a6cbec77ad81398
snehasaraswat8146-hash <snehasaraswat.8146@gmail.com>
2026-06-05 01:06:01 +0530

    Add files via upload

---
+++ day4_questions/day4_q13.c
@@ -0,0 +1,16 @@
+// Write a program to generate fibonacci series
+#include <stdio.h>
+int main()
+{
+ int a=0,b=1,c,i,n;
+ printf("enter the number:");
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+{
+ printf("%d",a);
+ c=a+b;
+ a=b;
+ b=c;
+}
+ return 0;
+}
