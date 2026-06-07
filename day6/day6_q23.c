7158e223f67b67f57794f0db5ad42f1a5266ffb9
snehasaraswat8146-hash <snehasaraswat.8146@gmail.com>
2026-06-07 08:19:26 +0530

    Add files via upload

--- day6/day6_q23.c
+++ day6/day6_q23.c
@@ -1,20 +1,30 @@
 //Write a program to count set bits in a number
 #include <stdio.h>
+
 int main()
 {
-    int n, count=0;
-    printf("enter the number:");
-    scanf("%d",&n);
-    if(n<0){
-        printf("enter a positive number");
+    int n, count = 0;
+
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    if(n < 0)
+    {
+        printf("Enter a positive number");
         return 0;
     }
-    while(n>0){
-        if(n%2==1)
+
+    while(n > 0)
+    {
+        if((n & 1) == 1)
         {
             count++;
         }
-        n=n/2;
+
+        n = n >> 1;
     }
-    printf("the number of set bits is: %d", count);
-    return 0;}
+
+    printf("The number of set bits is: %d", count);
+
+    return 0;
+}
