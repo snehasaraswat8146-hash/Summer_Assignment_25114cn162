d69d2014f230285efc5d83d45f6e107769f51736
snehasaraswat8146-hash <snehasaraswat.8146@gmail.com>
2026-06-08 20:58:25 +0530

    Add files via upload

---
+++ day 8/day8_q31.c
@@ -0,0 +1,15 @@
+// Write a program to print a character triangle
+#include <stdio.h>
+int main()
+{
+int i,j,n;
+  printf("enter the number:");
+  scanf("%d",&n);
+ for(i=1;i<=n;i++){
+    for(j=1;j<=i;j++){
+       printf("%c",'A'+(j-1));
+     }
+    printf("\n");
+}
+return 0;
+}
---
+++ day 8/day8_q32.c
@@ -0,0 +1,16 @@
+// Write a program to print a repeated-number pattern
+
+#include <stdio.h>
+int main()
+{
+int i,j,n;
+  printf("enter the number:");
+  scanf("%d",&n);
+ for(i=1;i<=n;i++){
+    for(j=1;j<=i;j++){
+       printf("%d",i);
+     }
+    printf("\n");
+}
+return 0;
+}
