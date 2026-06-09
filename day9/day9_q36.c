c7967dd94b0b6af8fbb2d05122b480bfb9442f8d
snehasaraswat8146-hash <snehasaraswat.8146@gmail.com>
2026-06-09 14:32:05 +0530

    Add files via upload

---
+++ day9/day9_q36.c
@@ -0,0 +1,21 @@
+// Write a program to print a hollow square pattern
+#include <stdio.h>
+int main()
+{
+int i,j,n;
+  printf("enter the number:");
+  scanf("%d",&n);
+ for(i=1;i<=n;i++){
+    for(j=1;j<=n;j++){
+       if(i==1 || i==n || j==1 || j==n){
+       printf("*");
+     }
+       else{
+        printf(" ");
+    }
+}
+    printf("\n");
+
+ }
+return 0;
+}
